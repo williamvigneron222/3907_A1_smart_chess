dfe235d381b847aa260562a8a827ae04cd28204b
laurakentsa <laurakentsa@gmail.com>
2026-03-16 09:19:38 -0400

    checkpoint

--- smartchess/main/crypto/ASCON-AEAD128.h
+++ smartchess/main/crypto/ASCON-AEAD128.h
@@ -10,8 +10,12 @@
 #include <stdint.h> // uint64_t
 
 // Right Rotate uint64_t x by i bits
+<<<<<<< HEAD
 #define ROTR(x, i) (x>>i)^(x<<(64-i))
 
+=======
+#define ROTR(x, i) (((x) >> (i)) ^ ((x) << (64 - (i))))
+>>>>>>> fa9c835 (checkpoint)
 
 /////////////////////////
 // Global Constants
@@ -38,6 +42,10 @@ static const uint64_t roundConstants[16] = {
     0x000000000000005a,
     0x000000000000004b,
 };
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /////////////////////////
 // Function Declarations
@@ -110,5 +118,8 @@ void ascon_encrypt(uint64_t key[2], uint64_t nonce[2], uint64_t *ad, unsigned ad
 void ascon_decrypt(uint64_t key[2], uint64_t nonce[2], uint64_t *ad, unsigned adlen, uint64_t *p, 
     unsigned plen, uint64_t *c, uint64_t tag[2]);
 
+    #ifdef __cplusplus
+}
+#endif
 
 #endif /// ASCON_AEAD128_H
