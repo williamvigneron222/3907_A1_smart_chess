874e2fb40373005c16fee6dc0a3aabf8e516625e
laurakentsa <laurakentsa@gmail.com>
2026-03-18 10:41:18 -0400

    “checkpoint”

--- smartchess/main/main.cpp
+++ smartchess/main/main.cpp
@@ -1,24 +1,14 @@
 #include "Arduino.h"
 #include "Preferences.h"
-#include "esp_timer.h" // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/esp_timer.html
+#include "esp_timer.h"
 
+//#include "infared.h"
+#include "communication.h"
 
-// #include "adc.h"
-#include "infared.h"
-
-void setup()
+extern "C" void app_main(void)
 {
-    // Serial.begin(115200); /// IFDEF DEBUG  ?
-    irSetup();
+    
+
+    //irSetup();
+    communication_setup();
 }
-
-
-void loop()
-{
-    uint64_t h[4] = { 0 };
-    uint64_t m[2] = { 0, 1 }; // mlen 2
-    hash(m, 2, h);
-    //if IR button is held
-        irLoop();
-}
-
